1812b27c7562c2b314552229202ea68d923eb4a3
madallina <goncearucmadalina@yahoo.com>
2023-10-18 16:49:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchBtn = document.getElementById("search-btn");
+const mealList = document.getElementById("meal");
+const mealDetailsContent = document.querySelector(".meal-details-content");
+const recipeCloseBtn = document.getElementById("recipe-close-btn");
+
+//event listeners
+searchBtn.addEventListener("click", getMealList);
+
+//functions
+function getMealList() {
+  let searchInputTxt = document.getElementById("search-input").value.trim();
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          html += `<div class="meal-item" data-id="${meal.idMeal}">
+                <div class="meal-img">
+                  <img src="${meal.strMealThumb}" alt="food" />
+                </div>
+                <div class="meal-name">
+                  <h3>${meal.strMeal}</h3>
+                  <a href="#" class="recipe-btn">Get Recipe</a>
+                </div>
+              </div>`;
+        });
+      }
+      mealList.innerHTML=html;
+    });
+}
